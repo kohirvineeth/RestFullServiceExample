116a6e6e60266b2ba705ea43eaad2f163363bbe6
vineeth924 <vineethkumar924@gmail.com>
2023-08-20 22:19:25 +0530

    Restfullserviceexample with out jpa repository extends testing done with postman

---
+++ src/main/java/com/spring/sample/RestfullServiceExample/RestfullServiceExampleApplication.java
@@ -0,0 +1,13 @@
+package com.spring.sample.RestfullServiceExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullServiceExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfullServiceExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/sample/RestfullServiceExample/RestfullServiceExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.sample.RestfullServiceExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullServiceExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
